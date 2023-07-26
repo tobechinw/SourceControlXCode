f99fccc6df9550d5491fea1fa555b0ea7fb74f3c
Tobechi <tobechid6@gmail.com>
2023-07-26 15:10:26 -0400

    Initial Commit

---
+++ SourceControlXCode/SourceControlXCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SourceControlXCodeApp.swift
+//  SourceControlXCode
+//
+//  Created by Tobechi Nwachukwu on 2023-07-26.
+//
+
+import SwiftUI
+
+@main
+struct SourceControlXCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
